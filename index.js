af5b93910dab838a78e8aa90df748bbb0bacce58
Jose Ordaz <145297953+Odz17@users.noreply.github.com>
2023-11-10 14:26:12 +0100

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+     
+  }
 
-  add(item) {}
+  add(item) {
+    
+    this.items.push(item);
+    this.items.sort((a, b) => a -b);
+    this.length = this.items.length;
 
-  get(pos) {}
+    
+  
+  }
 
-  max() {}
+  get(pos) {
+    if (pos < 0 || pos >= this.length) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  min() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items[this.length - 1]; 
+    }
 
-  sum() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    else {
+    return this.items[0];
+    }
+  }
 
-  avg() {}
+  sum() {
+    return this.items.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+  }
+
+  avg() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
